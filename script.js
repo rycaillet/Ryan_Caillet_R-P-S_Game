02847e4141dd292063a2d50b8f7fbc7fcf7d4a7e
Ryan Caillet <Ryancaillet1995@gmail.com>
2022-10-03 10:48:18 -0400

    added a randomized computer choice

--- script.js
+++ script.js
@@ -1,7 +1,17 @@
+const randomComputerChoice = (arr) => {
+  const randomIndex = Math.floor(Math.random() * arr.length)
+  const item = arr[randomIndex]
+  return item
+}
+
+const choices = ['Rock', 'Paper', 'Scissors']
+
+const result = randomComputerChoice(choices)
+
+console.log(result)
+
 let playerChoice = 0
 let computerChoice = 0
 let tie = 0
 
-const choices = ['Rock', 'Paper', 'Scissors']
-
-document.querySelector('#rock').addEventListener('click', function () {})
+// document.querySelector('#rock').addEventListener('click', function () {})
