a38e2c723aa52d88af9833504f348f947972fcb3
Ryan Caillet <Ryancaillet1995@gmail.com>
2022-10-03 11:30:46 -0400

    added some DOM elements

--- script.js
+++ script.js
@@ -1,17 +1,19 @@
-const randomComputerChoice = (arr) => {
-  const randomIndex = Math.floor(Math.random() * arr.length)
-  const item = arr[randomIndex]
+const randomChoice = (arr) => {
+  const randomChoice = Math.floor(Math.random() * arr.length)
+  const item = arr[randomChoice]
   return item
 }
 
 const choices = ['Rock', 'Paper', 'Scissors']
 
-const result = randomComputerChoice(choices)
+const computerChoice = randomChoice(choices)
 
-console.log(result)
+console.log(computerChoice)
 
 let playerChoice = 0
-let computerChoice = 0
 let tie = 0
 
-// document.querySelector('#rock').addEventListener('click', function () {})
+document.querySelector('#rock').addEventListener('click', function () {
+  if (playerChoice === 'Rock') return computerChoice
+})
+console.log(playerChoice)
