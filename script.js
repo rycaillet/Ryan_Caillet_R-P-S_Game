daf424126b3ca6afa31504b61e91212a42fa1fcd
Ryan Caillet <Ryancaillet1995@gmail.com>
2022-10-03 10:28:37 -0400

    added choices to global, trying to add event listener

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
-function animal(obj) {
-  return `This ${obj.color} ${obj.name} has ${obj.legs} legs.`
-}
-console.log()
+let playerChoice = 0
+let computerChoice = 0
+let tie = 0
+
+const choices = ['Rock', 'Paper', 'Scissors']
+
+document.querySelector('#rock').addEventListener('click', function () {})
